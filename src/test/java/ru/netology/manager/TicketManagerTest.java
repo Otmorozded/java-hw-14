422a6cd853d038fc339a431022ba369dc6ce532d
Evgeny Vasilev <jony.torrent@gmail.com>
2021-01-22 12:50:58 +0300

    test add

--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -3,6 +3,7 @@ package ru.netology.manager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.TicketOffer;
+import ru.netology.domain.TicketByFlightTimeAscComparator;
 import ru.netology.repository.TicketRepository;
 
 
@@ -15,11 +16,12 @@ public class TicketManagerTest {
 
     private TicketRepository repository = new TicketRepository();
     private TicketManager manager = new TicketManager(repository);
-    private Comparator<TicketOffer> comparator;
+    private TicketByFlightTimeAscComparator comparator = new TicketByFlightTimeAscComparator();
 
-    private TicketOffer ticket1 = new TicketOffer(1, 15000, "LED", "VKO", 60);
+
+    private TicketOffer ticket1 = new TicketOffer(1, 17000, "LED", "VKO", 60);
     private TicketOffer ticket2 = new TicketOffer(2, 20000, "LED", "VKO", 75);
-    private TicketOffer ticket3 = new TicketOffer(3, 16000, "LED", "VKO", 70);
+    private TicketOffer ticket3 = new TicketOffer(3, 14000, "LED", "VKO", 70);
     private TicketOffer ticket4 = new TicketOffer(4, 30000, "IKT", "SVO", 250);
     private TicketOffer ticket5 = new TicketOffer(5, 27000, "IKT", "SVO", 350);
     private TicketOffer ticket6 = new TicketOffer(6, 50000, "SVO", "VVO", 550);
@@ -38,12 +40,37 @@ public class TicketManagerTest {
     }
 
     @Test
-    void shouldSortByTime() {
+    void shouldFindAllBySameDirectionAndSortByTime() {
         TicketOffer[] actual = manager.findAll("LED", "VKO", comparator);
         TicketOffer[] expected = new TicketOffer[]{ticket1, ticket3, ticket2};
         assertArrayEquals(expected, actual);
         System.out.print(Arrays.toString(actual));
     }
+
+    @Test
+    void shouldFindOneOffer() {
+
+        TicketOffer[] actual = manager.findAll("SVO", "VVO", comparator);
+        TicketOffer[] expected = new TicketOffer[]{ticket6};
+        assertArrayEquals(expected, actual);
+        System.out.print(Arrays.toString(actual));
+
+    }
+
+    @Test
+    void shouldNotFindWithWrongDirection() {
+
+        TicketOffer[] actual = manager.findAll("IST", "VVO", comparator);
+        TicketOffer[] expected = new TicketOffer[]{};
+        assertArrayEquals(expected, actual);
+        System.out.print(Arrays.toString(actual));
+
+
+    }
+
+
+
+
 
     
 
